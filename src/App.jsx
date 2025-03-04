a419a6c182db5800ddd2f2239207bade588735d1
Ben Sayed <Ahmed8Sayed1010@gmail.com>
2025-03-04 19:23:18 +0200

    009

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import Footer from "./component/Footer/Footer";
 import Home from "./Home/Home";
 import Header from "./component/Header/Header";
-import AboutPage from "./About/About"; // استيراد صفحة AboutPage
-import { Routes, Route } from 'react-router-dom';  // استيراد Routes و Route من react-router-dom
+import AboutPage from "./About/About";
+import { Routes, Route } from 'react-router-dom';  
 
 function App() {
   return (
     <div className="all">
-      <Header /> {/* رأس الموقع */}
+      <Header />  
       
       <Routes>
-        {/* تحديد المسارات */}
-        <Route path="/" element={<Home />} />  {/* الصفحة الرئيسية */}
-        <Route path="/about" element={<AboutPage />} />  {/* صفحة من نحن */}
+       
+        <Route path="/" element={<Home />} />  
+        <Route path="/about" element={<AboutPage />} />  
       </Routes>
       
-      <Footer /> {/* الفوتر */}
+      <Footer /> 
     </div>
   );
 }
