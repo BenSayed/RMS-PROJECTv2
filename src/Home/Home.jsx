af44129306c1df9d2c8565f86803dfbe984c7764
Ben Sayed <Ahmed8Sayed1010@gmail.com>
2025-03-07 03:28:57 +0200

    add section2

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
   return (
     <div className='HomeAllSection'>
       <Section1/>
-      {/* <Section2/>
-      <Section3/>
+      <Section2/>
+      {/* <Section3/>
       <Section4/>
       <Section5/> */}
     </div>
