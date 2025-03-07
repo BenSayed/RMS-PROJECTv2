20b3b09884f739155473157c2f18b9bfbe4efbd4
Ben Sayed <Ahmed8Sayed1010@gmail.com>
2025-03-07 06:10:49 +0200

    edit of section 4

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
       <Section1/>
       <Section2/>
       <Section3/>
-      {/* <Section4/>
-      <Section5/> */}
+      <Section4/>
+      {/* <Section5/> */}
     </div>
   );
 }
--- src/Home/section-4/section4.jsx
+++ src/Home/section-4/section4.jsx
@@ -5,24 +5,56 @@ import image32 from "./2.svg";
 import image33 from "./3.svg";
 import image34 from "./4.svg";
 import image35 from "./Vector (1).svg";
+import image36 from "./Vector@2x.svg";
 import { Link } from "react-router-dom";
+
+const foodItems = [
+  {
+    id: 1,
+    image: image31,
+    title: "Wagyu Steak",
+    description: "250g of lean steak with sous and smashed potato or rice",
+    price: "45$",
+    rating: 5,
+  },
+  {
+    id: 2,
+    image: image32,
+    title: "Wagyu Steak",
+    description: "250g of lean steak with sous and smashed potato or rice",
+    price: "45$",
+    rating: 5,
+  },
+  {
+    id: 3,
+    image: image33,
+    title: "Wagyu Steak",
+    description: "250g of lean steak with sous and smashed potato or rice",
+    price: "45$",
+    rating: 5,
+  },
+  {
+    id: 4,
+    image: image34,
+    title: "Wagyu Steak",
+    description: "250g of lean steak with sous and smashed potato or rice",
+    price: "45$",
+    rating: 5,
+  },
+];
 
 const Section4 = () => {
   return (
     <div>
-      <section >
-         <div className="section4"> 
-
-
+      <section>
+        <div className="section4">
           <div className="food-selection">
             <h2>
               Choose the type of <span style={{ color: "#ff9500" }}>Food</span>
             </h2>
           </div>
-
-
-
-          <div className="navbar22">
+ <div className="navbarscroll"> 
+ <div className="navbar22">
             <button>Main Courses</button>
             <button>Appetizers</button>
             <button>Seafood</button>
@@ -30,94 +62,49 @@ const Section4 = () => {
             <button>Beverages</button>
           </div>
 
+ </div>
+          
 
-
+        <div className="cardsection"> 
           <div className="cards">
-            <div className="card">
-              <img src={image31} alt="Wagyu Steak" />
-              <div className="content">
-                <div className="text">
-                  <h3>Wagyu Steak</h3>
-                  <p>
-                    Wagyu Steak Wagyu Steak <br />
-                    Steak Wagyu Steak
-                  </p>
-                  <div className="stars">
-                    &#9733; &#9733; &#9733; &#9733; &#9733;
+            {foodItems.map((food) => (
+              <div className="card" key={food.id}>
+                <img src={food.image} alt={food.title} />
+                <div className="content">
+                  <div className="cardtextandheart">
+                    <h3>{food.title}</h3>
+                    <img src={image36} alt="Heart Icon" />
                   </div>
-                </div>
-                <div className="price">68$</div>
-              </div>
-            </div>
 
-            <div className="card">
-              <img src={image32} alt="Lobster Roll" />
-              <div className="content">
-                <div className="text">
-                  <h3>Wagyu Steak</h3>
-                  <p>
-                    Wagyu Steak Wagyu Steak <br />
-                    Steak Wagyu Steak
-                  </p>
-                  <div className="stars">
-                    &#9733; &#9733; &#9733; &#9733; &#9733;
+                  <div className="cardpargraph">
+                    <p>{food.description}</p>
                   </div>
-                </div>
-                <div className="price">68$</div>
-              </div>
-            </div>
 
-            <div className="card">
-              <img src={image33} alt="Pasta Carbonara" />
-              <div className="content">
-                <div className="text">
-                  <h3>Wagyu Steak</h3>
-                  <p>
-                    Wagyu Steak Wagyu Steak <br />
-                    Steak Wagyu Steak
-                  </p>
-                  <div className="stars">
-                    &#9733; &#9733; &#9733; &#9733; &#9733;
+                  <div className="cardhandelstartandprise">
+                    <div className="stadprisersan">
+                      {Array(food.rating)
+                        .fill()
+                        .map((_, i) => (
+                          <span key={i}>&#9733;</span>
+                        ))}
+                    </div>
+                    <h2>{food.price}</h2>
                   </div>
                 </div>
-                <div className="price">68$</div>
               </div>
-            </div>
-
-            <div className="card">
-              <img src={image34} alt="Cheesecake" />
-              <div className="content">
-                <div className="text">
-                  <h3>Wagyu Steak</h3>
-                  <p>
-                    Wagyu Steak Wagyu Steak <br />
-                    Steak Wagyu Steak
-                  </p>
-                  <div className="stars">
-                    &#9733; &#9733; &#9733; &#9733; &#9733;
-                  </div>
-                </div>
-                <div className="price">68$</div>
-              </div>
-            </div>
+            ))}
           </div>
+          </div>
+
           <div className="buutonshowall">
-            <Link to="/menu" className="show-all-button" >
-            <span>Show All</span>
-            
-            <div className="icon-container">
-            <img src={image35} alt="" />
-        
-                </div>
-                      </Link>
-      </div>
+            <Link to="/menu" className="show-all-button">
+              <span>Show All</span>
+              <div className="icon-container">
+                <img src={image35} alt="Arrow Icon" />
+              </div>
+            </Link>
           </div>
-
-    
-
-           
-         
-         
+        </div>
       </section>
     </div>
   );
